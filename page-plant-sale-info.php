b5b9acb88b3ca5f914132781f8bc6de9d39abcee
chris <clcooley@umich.edu>
2020-12-21 23:38:51 -0500

    basic outline for plant-sale-info

--- page-plant-sale-info.php
+++ page-plant-sale-info.php
@@ -2,11 +2,84 @@
   get_header(); 
 ?>
 
-<h1>Add your message here</h1>
+<div class="header-img">
+    <div class="center-header-box">
+        <h1 id="header-title">Plant Sale Information</h1>
+        <button class="plant-catalog-btn"><b>Plant Catalog</b></button>
+    </div>
+</div>
+<div class="indent main-page title">Project Grow Plant Sale</div>
+<div class="indent description">
+    Every year, Project Grow holds a plant sale offering hard to find heirloom tomatoes, peppers, and basil. This year, Project Grow is offering transplants of six types (and 75 varieties) of tomatoes, 12 varieties of hot peppers, 11 different sweet peppers, and Genovese and Thai basil.
+</div>
+<div class="sale-container">
+    <div class="sale-headers">
+        <div><b>Sale Date</b></div>
+        <div><b>Price</b></div>
+        <div><b>Size</b></div>
+    </div>
+    <div class="sale-contents">
+        <div class="dates">
+            <div class="sale-date">
+                <br/>
+                <div><i>Friday</i></div>
+                <br/>
+                <div><b>May 8th</b></div>
+                <br/>
+                <div>4pm - 8pm</div>
+            </div>
+            <div class="sale-date">
+                <br/>
+                <div><i>Saturday</i></div>
+                <br/>
+                <div><b>May 9th</b></div>
+                <br/>
+                <div>10am - 1pm</div>
+            </div>
+        </div>
+        <div class="prices">
+            <br/>
+            <div><b>$3.00</b></div>
+            <div><i>per plant</i></div>
+        </div>
+        <div class="sizes"><br/><b>3 1/2 inch plot</b></div>
+    </div>
+</div>
+<div class="organic-container">
+    <div class="indent organic title">Organic</div>
+    <div class="indent organic-paragraph">
+        The plants are grown in USDA Organic Sunshine Mix. However, they are fertilized with Fertrell 3-2-3 Garden Food. 
+        Fertrell is a natural fertilizer made of blood meal, feather meal and other natural products, but is not USDA organic 
+        because the blood, feathers and other products are not from organically raised animals. We use this fertilizer only 
+        after having tried other organically certified fertilizers in the past, which produced unsatisfactory results. Whenever 
+        possible, the plants are grown from seeds saved by local gardeners.
 
-<form class="message-form" method="post">
-  <input id="message-title" type="text">
-  <button  type="submit">Save!</button>
-</form>
+    </div>
+</div>
+<div class="new-items-container">
+    <div class="new-items title">New Items</div>
+    <div class="new-items-body">
+
+    </div>
+</div>
+<div class="discont-items-container">
+    <div class="discont-items title">Discontinued Items</div>
+    <div class="discont-items-body">
+
+    </div>
+</div>
+<div class="disclaimer-container">
+    <div class="indent disclaimer title">Disclaimer</div>
+    <div class="indent disclaimer-body">
+        To make this work, we ask that our customers follow a few simple requests. 
+        Please read these limitations carefully, and if you cannot meet these requirements, 
+        do not advance order plants, but come to the sale in person to make your purchases!
+        <br/><b>1.</b> You must pay for your order by check by Saturday May 2nd. Orders that are received 
+        later than May 2nd or are not paid for will not be pulled and saved for you.
+        <br/><b>2.</b> You must come to Dawn Farm to pick up your order. If you can't make it, ask a 
+        friend to pick up the order for you. Orders which are not picked up during the assigned 
+        days and hours will be lost and you will receive no refund.
+    </div>
+</div>
 
 <?php get_footer(); ?>
