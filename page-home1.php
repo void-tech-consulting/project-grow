258e2e732bfa83d6134b7594f5b5d55837d56a5e
bmalladi <bmalladi@umich.edu>
2020-12-23 15:49:38 -0800

    starting events stuff

--- page-home1.php
+++ page-home1.php
@@ -1,36 +1,29 @@
-<html lang="en">
+<?php
+get_header();
+?>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-</head>
-
-<body>
-
-    <?php
-    get_header();
-    ?>
-
-    <div class="banner">
-        <div class="banner-text">
-            <div class="banner-head">
-                Welcome to Project Grow
-            </div>
-            Project Grow Community Gardens provides the space, education, and inspiration to make organic gardening accessible to all.
-            <br><br>
-            Rent a plot at one of our wonderful community garden sites, meet a diverse group of gardeners, and reap the rewards of your hard work with fresh, healthy, and beautiful plants!
-            <br>
-            <div class="apply-plot">
-                Apply for a Plot
-            </div>
-
+<div class="banner">
+    <div class="banner-text">
+        <div class="banner-head">
+            Welcome to Project Grow
+        </div>
+        Project Grow Community Gardens provides the space, education, and inspiration to make organic gardening accessible to all.
+        <br><br>
+        Rent a plot at one of our wonderful community garden sites, meet a diverse group of gardeners, and reap the rewards of your hard work with fresh, healthy, and beautiful plants!
+        <br>
+        <div class="apply-plot">
+            Apply for a Plot
         </div>
     </div>
+</div> <!-- end banner section -->
 
-    <?php get_footer(); ?>
-</body>
+<div class="events">
+    <div class="event-text">
+        event text
+    </div>
+    <img src="eventimg.png" alt="">
+</div>
 
-</html>
+<?php get_footer(); ?>
