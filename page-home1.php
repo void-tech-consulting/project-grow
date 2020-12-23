ea250174df65b84ae319fe1aa5adbdc9704ae520
bmalladi <bmalladi@umich.edu>
2020-12-23 15:17:07 -0800

    first draft banner

--- page-home1.php
+++ page-home1.php
@@ -1,12 +1,36 @@
-<?php
-get_header();
-?>
+<html lang="en">
 
-<h1>Add your message here</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
 
-<form class="message-form" method="post">
-    <input id="message-title" type="text">
-    <button type="submit">Save!</button>
-</form>
+<body>
 
-<?php get_footer(); ?>
+    <?php
+    get_header();
+    ?>
+
+    <div class="banner">
+        <div class="banner-text">
+            <div class="banner-head">
+                Welcome to Project Grow
+            </div>
+            Project Grow Community Gardens provides the space, education, and inspiration to make organic gardening accessible to all.
+            <br><br>
+            Rent a plot at one of our wonderful community garden sites, meet a diverse group of gardeners, and reap the rewards of your hard work with fresh, healthy, and beautiful plants!
+            <br>
+            <div class="apply-plot">
+                Apply for a Plot
+            </div>
+
+        </div>
+    </div>
+
+    <?php get_footer(); ?>
+</body>
+
+</html>
