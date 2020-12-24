4377648b43ff38ff0da6e108d685956926747a79
chris <clcooley@umich.edu>
2020-12-24 14:11:42 -0500

    Ready for feedback
    Advice for mobile responsiveness
    --should the table go vertical?
    
    How should the brown box scale as it gets smaller?

--- page-plant-sale-info.php
+++ page-plant-sale-info.php
@@ -5,20 +5,21 @@
 <div class="header-img">
     <div class="center-header-box">
         <h1 id="header-title">Plant Sale Information</h1>
+        <!--Make button actually link to other page eventually-->
         <button class="plant-catalog-btn"><b>Plant Catalog</b></button>
     </div>
 </div>
-<div class="indent main-page title">Project Grow Plant Sale</div>
+<div class="indent main-page title">Project Grow Plant Sale</div>   
 <div class="indent description">
     Every year, Project Grow holds a plant sale offering hard to find heirloom tomatoes, peppers, and basil. This year, Project Grow is offering transplants of six types (and 75 varieties) of tomatoes, 12 varieties of hot peppers, 11 different sweet peppers, and Genovese and Thai basil.
 </div>
 <div class="sale-container">
-    <div class="sale-headers">
-        <div><b>Sale Date</b></div>
+    <div class="sale-headers sale-flex-format">
+        <div class="sale-table-format"><b>Sale Date</b></div>
         <div><b>Price</b></div>
-        <div><b>Size</b></div>
+        <div class="sale-table-format"><b>Size</b></div>
     </div>
-    <div class="sale-contents">
+    <div class="sale-flex-format">
         <div class="dates">
             <div class="sale-date">
                 <br/>
@@ -42,12 +43,12 @@
             <div><b>$3.00</b></div>
             <div><i>per plant</i></div>
         </div>
-        <div class="sizes"><br/><b>3 1/2 inch plot</b></div>
+        <div class="sale-table-format sizes"><br/><b>3 1/2 inch plot</b></div>
     </div>
 </div>
-<div class="organic-container">
-    <div class="indent organic title">Organic</div>
-    <div class="indent organic-paragraph">
+<div class="indent organic-container">
+    <div class="section-spacer title">Organic</div>
+    <div class="organic-paragraph">
         The plants are grown in USDA Organic Sunshine Mix. However, they are fertilized with Fertrell 3-2-3 Garden Food. 
         Fertrell is a natural fertilizer made of blood meal, feather meal and other natural products, but is not USDA organic 
         because the blood, feathers and other products are not from organically raised animals. We use this fertilizer only 
@@ -56,13 +57,13 @@
 
     </div>
 </div>
-<div class="new-items-container">
-    <div class="indent new-items title">New Items</div>
-    <div class="indent hdr-over-list">
+<div class="indent new-items-container">
+    <div class="section-spacer title">New Items</div>
+    <div class="hdr-over-list">
         Every year Project Grow strives to replace the least popular items with new varieties we have not offered before or think 
         should be grown more often. This year we are adding:<br/>
     </div>
-    <div class="indent new-items-body">
+    <div class="flex-container">
         <div class="new-items-left">
             <b>Tomatoes</b><br/>
             - Aunt Ruby's German Green (back!)<br/>
@@ -84,13 +85,13 @@
     </div>
 </div>
 <div class="indent discont-items-container">
-    <div class="discont-items title">Discontinued Items</div>
+    <div class="section-spacer title">Discontinued Items</div>
     <div class="hdr-over-list">
         The following items have been dropped because of poor sales, difficulty finding good seed, or work 
         required to grow. If you were particularly fond of any of these, let us know,  we may be able to 
         sell you seed so you can grow your own.
     </div>
-    <div class="discont-items-body">
+    <div class="flex-container">
         <div class="discont-items-left">
             <b>Tomatoes</b><br/>
             - Cherokee Chocolate (replaced by Black Brandywine)<br/>
@@ -128,9 +129,9 @@
             recommendations.
         </div>
 </div>
-<div class="disclaimer-container">
-    <div class="indent disclaimer title">Disclaimer</div>
-    <div class="indent disclaimer-body">
+<div class="indent disclaimer-container">
+    <div class="section-spacer title">Disclaimer</div>
+    <div class="disclaimer-body">
         To make this work, we ask that our customers follow a few simple requests. 
         <b>Please read these limitations carefully</b>, and if you cannot meet these requirements, 
         do not advance order plants, but come to the sale in person to make your purchases!
