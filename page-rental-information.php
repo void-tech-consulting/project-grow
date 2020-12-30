2207ee6beb13cf406e441fac13a81e5d484144d0
wongm8 <wongm8@bxscience.edu>
2020-12-30 18:32:05 -0500

    Rental information layout
    Finished layout based on figma, still need to add green bar and simplify class names

---
+++ page-rental-information.php
@@ -0,0 +1,25 @@
+<?php 
+  get_header(); 
+?>
+
+
+<span>Important Dates</span>
+<div class="important-dates-timeline-container">
+  <div class="important-dates-timeline-item">
+    <img src="http://project-grow.local/wp-content/uploads/2020/12/timeline_pic_1.png" alt="">
+  </div>
+  <div class="important-dates-timeline-item">
+    <img src="http://project-grow.local/wp-content/uploads/2020/12/timeline_pic_2.png" alt="">
+  </div>
+  <div class="important-dates-timeline-item">
+  <img src="http://project-grow.local/wp-content/uploads/2020/12/timeline_pic_3.png" alt="">
+  </div>
+  <div class="important-dates-timeline-item">
+  <img src="http://project-grow.local/wp-content/uploads/2020/12/timeline_pic_4.png" alt="">
+  </div>
+  <div class="important-dates-timeline-item">
+  <img src="http://project-grow.local/wp-content/uploads/2020/12/timeline_pic_5.png" alt="">
+  </div>
+</div>
+
+<?php get_footer(); ?>
