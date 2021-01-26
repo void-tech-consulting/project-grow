dac578cc5388b4be8bb0eca9ba8c02ef63bbca13
KavyaManocha <59888688+KavyaManocha@users.noreply.github.com>
2021-01-25 22:43:30 -0500

    Add files via upload
    finished code for one event section in functions.php and edited page-classes-events to make customizable

--- page-classes-events.php
+++ page-classes-events.php
@@ -1,12 +1,17 @@
 <?php
   get_header();
 ?>
+<style>
+    <?php include 'CSS/main.css'; ?>
+</style>
 <div class="header-img">
     <div class="center-header-box">
         <h1 id="header-title">Classes & Events</h1>
     </div>
 </div>
-<div class="indent main-page title"><b>Classes & Events</b></div> 
+<div class="indent main-page title"><b>Classes & Events</b></div>
+
+
 
 <!--Start code for event page-->
 <div class="event-container">
@@ -18,38 +23,48 @@
         <div class="event-details">
             <div class="event-what-where">
                 <div class="event-title">
-                    <b>Seed Share</b>
+                    <!-- used to be "Seed Share" heading  -->
+                    <b><?php echo get_theme_mod('classes-events-headline')?></b>
                 </div>
                 <div id="event-where">
-                    <b>Downtown Home and Garden</b>
+                    <!-- used to be <b>Downtown Home and Garden</b> -->
+                    <b><?php echo get_theme_mod('classes-events-location')?></b>
+                    
                 </div>
                 <div id="class-or-event">
-                    <u>Event</u>
+                    <!-- Used to be <u>Event</u> -->
+                    <u><?php echo get_theme_mod('classes-events-event-type')?></u>
                 </div>
             </div>
             <div class="event-when">
                 <div class="event-date">
-                    March 21, 2020
+                    <!-- March 21, 2020 -->
+                    <?php echo get_theme_mod('classes-events-date')?>
                 </div>
                 <div class="event-time">
-                    10:00AM - 11:30AM
+                    <!-- 10:00AM - 11:30AM -->
+                    <?php echo get_theme_mod('classes-events-time')?>
                 </div>
             </div>
         </div>
         <div class="event-description">
-            <p>Did you save more seeds than you could ever use? 
-                Did you perhaps go a bit mad at the end-of-year 
-                seed sales? Then the Seed Share is for you! Why 
-                not see what you can exchange with other gardeners? 
-                Who knows, perhaps you'll find seeds for that 
-                delicious corn variety you couldn't buy enough of 
-                at the farmer's market! Please do not park in the 
+            <?php echo wpautop(get_theme_mod('classes-events-paragraph'))?>
+            <!-- <p>Did you save more seeds than you could ever use?
+                Did you perhaps go a bit mad at the end-of-year
+                seed sales? Then the Seed Share is for you! Why
+                not see what you can exchange with other gardeners?
+                Who knows, perhaps you'll find seeds for that
+                delicious corn variety you couldn't buy enough of
+                at the farmer's market! Please do not park in the
                 Downtown Home and Garden parking lot for this event.
-            </p>
+            </p> -->
         </div>
         <div class="event-cost-join">
             <div class="event-cost">
-                Cost: $5.00
+                Cost:$
+            </div>
+            <div class = "event-cost">
+                <?php echo get_theme_mod('classes-events-cost')?>
             </div>
             <button class="register-event">Join</button>
         </div>
