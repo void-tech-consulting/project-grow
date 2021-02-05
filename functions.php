85604f4d700da06267911c1038bdefad2874ba5d
Alvin Chen <alvinch@umich.edu>
2021-02-05 14:11:35 -0500

    updated functions.php for custom repeater

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 require get_template_directory() . '/inc/example-post-type.php';
 require get_template_directory() . '/inc/enqueue-scripts.php';
 require get_template_directory() . '/inc/register-settings.php';
+require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/template_functions.php';
 ?>
 
 <?php
