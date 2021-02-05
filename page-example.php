ccc2f5b8c80bd769929b907a5cc9491fcd558415
Alvin Chen <alvinch@umich.edu>
2021-02-05 15:32:27 -0500

    added repeater to page-example

--- page-example.php
+++ page-example.php
@@ -14,9 +14,37 @@
   <p>Lorem ipsum...</p>
 </div>
 
-<div class="editable">
-    <h1 class="title"><?php echo get_theme_mod('title')?></h1>
-    <p class="desc"><?php echo get_theme_mod('description')?></p>
+<div>
+    <?php
+      require 'inc/section_vars.php';   
+      // get_example_data is in /inc/template_functions.php
+      $data  = get_example_data($example_repeater);
+      if(!empty( $data ) ) { 
+        ?>
+        <section class="example">
+        <?php
+          foreach ( $data as $k => $f ) {  
+            // Make sure to use a semicolon; when using php on multiple lines
+            $questionId = 'question'.$k;
+            $answerContent = "<div id=\"".$questionId."\" class=\"answer-text\">";
+            ?>
+            <div class="questionbox" <?php echo "data-question-id=\"".$k."\"" ?>> 
+              <span class="question-text"><?php echo $f['question']?> </span>
+            </div>
+            <div class="dropdown-line"></div>
+            <?php echo $answerContent ?>
+              <a href="<?php echo $f['link']; ?>">
+                <div class= "answer-wrap">
+                    <?php echo $f['answer']; ?>
+                </div>
+              </a>
+            </div>
+      <?php
+          }
+      ?>
+        </section>
+<?php } ?>
+
 </div>
 
 <?php get_footer(); ?>
