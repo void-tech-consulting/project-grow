1fd73a17094286715cb10267d17bca1162858f74
Christopher Cooley <clcooley@umich.edu>
2021-02-10 11:27:39 -0500

    Finished page-guidelines
    Left to do:
     - What goes in the maintenance box, it was not on old site
     - Need to get the Member Handbook upload to link to
    
     Also included in push:
     - Moved customizer for classes-event to register-settings

--- page-classes-events.php
+++ page-classes-events.php
@@ -51,15 +51,6 @@
             </div>
             <div class="event-description">
                 <?php echo wpautop(get_theme_mod('classes-events-paragraph'))?>
-                <!-- <p>Did you save more seeds than you could ever use?
-                    Did you perhaps go a bit mad at the end-of-year
-                    seed sales? Then the Seed Share is for you! Why
-                    not see what you can exchange with other gardeners?
-                    Who knows, perhaps you'll find seeds for that
-                    delicious corn variety you couldn't buy enough of
-                    at the farmer's market! Please do not park in the
-                    Downtown Home and Garden parking lot for this event.
-                </p> -->
             </div>
             <div class="event-cost-join">
                 <div class="event-cost">
--- page-plot-guidelines.php
+++ page-plot-guidelines.php
@@ -59,7 +59,7 @@
     </div>
     <!--All three expansion boxes go here-->
     <div id="weedGuide" class="guidelines-expansion hide left">
-      <div id="weed-top-text">
+      <div class="guideline">
         <p>
           Controlling weeds not only helps your plants flourish, but it 
           prevents neighbor garden plots from becoming overrun as well
@@ -75,7 +75,7 @@
           to human health, may kill or damage desirable crops, drift into 
           neighboring gardens, or remain in the soil long after initial use
         </p>
-        <div id="weed-bottom-text">
+        <div id="weed-examples">
           <div id="weed-fungicides">
             <p>
               <b>Allowed fungicides:</b></br>
@@ -104,12 +104,38 @@
         </div>
       </div>
     </div>
+
     <div id="insectGuide" class="guidelines-expansion hide middle">
-      TODO
+      <div class="guideline">
+        <p>
+          Controlling insects is important to your own garden health, 
+          but also helps your neighbors prevent excessive damage as well
+        </p>
+        <p>
+          <b>Recommended:</b> Companion planting, fabric row covers, biological 
+          controls (like Bacillus thuringiensis - a bacterium that controls 
+          cabbage worms), and introducing natural predator species (like 
+          ladybugs and lacewings); as a last resort natural botanical pesticides 
+          (like Rotenone and Pyrethrum) that break down quickly and do not 
+          leave harmful residue in the soil may be used
+        </p>
+        <p>
+          <b>Prohibited:</b> Synthetic pesticides of any kind
+        </p>
+      </div>
     </div>
+
     <div id="diseaseGuide" class="guidelines-expansion hide right">
-      TODO2
+      <div class="guideline">
+        <p>
+          Most problems can be controlled by maintaining a clean garden and a healthy soil
+        </p>
+        <p>
+          <b>Recommended:</b> Choose disease-resistant plant varieties, water judiciously, and remove diseased plants from the garden
+        </p>
+      </div>
     </div>
+
     <div class="indent" id="guidelines-row">
       <div id="fertilizer" onclick="print_guide(fertilizer, fertilizerGuide)" class="story-box">
         <p>
@@ -133,57 +159,61 @@
     </div>
     <!--All of the dropdowns go here-->
     <div id="fertilizerGuide" class="guidelines-expansion hide left">
-      <div id="weed-top-text">
+      <div class="guideline">
         <p>
-          Controlling weeds not only helps your plants flourish, but it 
-          prevents neighbor garden plots from becoming overrun as well
+          Soil health is important not only for your plants this season, 
+          but for future gardeners as well; therefore, Project Grow strongly 
+          encourages adding ample organic material (i.e. compost, leaf mold, 
+          manure) maintain ongoing soil fertility
         </p>
         <p>
-          <b>Recommended:</b> Use hay to mulch around established seedlings 
-          then hand weed as needed (Note: many sites coordinate a hay 
-          delivery by a local farmer at the beginning of the season - 
-          ask your Site Coordinator for details)
+          <b>Recommended:</b> Products of plant or animal origin - compost, 
+          leaf mold, livestock manure, blood and bone meal, cover crops, fish 
+          emulsion, kelp meal, liquid seaweed; commercial organic fertilizer 
+          blends;  natural mineral fertilizers - greensand, granite dust, ground limestone
         </p>
         <p>
-          <b>Prohibited:</b> Herbicides of any kind as they are hazardous 
-          to human health, may kill or damage desirable crops, drift into 
-          neighboring gardens, or remain in the soil long after initial use
+          <b>Prohibited:</b> Inorganic fertilizers (e.g. 5-1-5, Miracle Grow)
         </p>
-        <div id="weed-bottom-text">
-          <div id="weed-fungicides">
-            <p>
-              <b>Allowed fungicides:</b></br>
-              Baking soda</br>
-              Liquid copper</br>
-              Anything else specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed fungicides:</b></br>
-              Anything not specifically labeled organic.
-            </p>
-          
-          </div>
-          <div id="weed-herbicides">
-            <p>
-              <b>Allowed herbicides:</b></br>
-              Anything specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed herbicides:</b></br>
-              Round Up (glyphosate)</br>
-              Preen</br>
-              Anything not specifically labeled organic.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
+
     <div id="compostGuide" class="guidelines-expansion hide middle">
-      compost
+      <div class="guideline">
+        <p>
+          Project Grow strongly encourages composting organic material from plots, 
+          but it can only be done in designated areas at garden sites
+        </p>
+        <p>
+          <b>Recommended:</b> Check your site's addendum in the 
+          <a href="google.com">Member Handbook</a>
+          for the composting policy and on-site composting location (if available)
+        </p>
+        <p>
+          <b>Prohibited:</b> Composting garden refuse at the garden site outside of specified areas
+        </p>
+      </div>
     </div>
+
     <div id="forbiddenGuide" class="guidelines-expansion hide right">
-      forbidden
+      <div class="guideline">
+        <p>
+          In the spirit of community gardens, please ensure that your plot is 
+          free from unacceptable plants; if you have concerns about neighboring 
+          sites, please contact your Site Coordinator or the Board
+        </p>
+        <p>
+          <b>Recommended:</b> If you find an unacceptable plant in your plot 
+          (e.g. an invasive species like Mint), do not move it - if you'd like 
+          to keep it for the season, limit its spread within your plot and do not transplant
+        </p>
+        <p>
+          <b>Prohibited:</b> Invasive species (e.g. mint, comfrey, Jerusalem artichoke); 
+          trees, bushes, or other woody perennials; illegal or dangerous plants
+        </p>
+      </div>
     </div>
+
     <div class="indent" id="guidelines-row">
       <div id="coords" onclick="print_guide(coords, coordsGuide)" class="story-box">
         <p>
@@ -206,56 +236,42 @@
     </div>
     <!--All dropdowns here-->
     <div id="coordsGuide" class="guidelines-expansion hide left">
-      <div id="weed-top-text">
+      <div class="guideline">
         <p>
-          Controlling weeds not only helps your plants flourish, but it 
-          prevents neighbor garden plots from becoming overrun as well
+          Site Coordinators are responsible for answering gardening questions 
+          and organizing opening day, work parties, plow dates, and closing day
         </p>
         <p>
-          <b>Recommended:</b> Use hay to mulch around established seedlings 
-          then hand weed as needed (Note: many sites coordinate a hay 
-          delivery by a local farmer at the beginning of the season - 
-          ask your Site Coordinator for details)
+          <b>Recommended:</b> Let your Site Coordinator know the best way 
+          to contact you and/or when your contact information changes
         </p>
-        <p>
-          <b>Prohibited:</b> Herbicides of any kind as they are hazardous 
-          to human health, may kill or damage desirable crops, drift into 
-          neighboring gardens, or remain in the soil long after initial use
-        </p>
-        <div id="weed-bottom-text">
-          <div id="weed-fungicides">
-            <p>
-              <b>Allowed fungicides:</b></br>
-              Baking soda</br>
-              Liquid copper</br>
-              Anything else specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed fungicides:</b></br>
-              Anything not specifically labeled organic.
-            </p>
-          
-          </div>
-          <div id="weed-herbicides">
-            <p>
-              <b>Allowed herbicides:</b></br>
-              Anything specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed herbicides:</b></br>
-              Round Up (glyphosate)</br>
-              Preen</br>
-              Anything not specifically labeled organic.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
     <div id="partiesGuide" class="guidelines-expansion hide middle">
-      parties
+      <div class="guideline">
+        <p>
+          Project Grow Community Gardens participants are encouraged 
+          to actively participate in their site's community
+        </p>
+        <p>
+          <b>Recommended:</b> Attend work parties to maintain common 
+          areas, get to know your neighboring gardeners, respect fellow 
+          gardeners by maintaining your own plot, help others and ask for help when you need it
+        </p>
+      </div>
     </div>
     <div id="toolsGuide" class="guidelines-expansion hide right">
-      tools
+      <div class="guideline">
+        <p>
+        Many sites supply a few gardening tools marked with a red 
+        handle and "Project Grow;" all sites supply hoses
+        </p>
+        <p>
+          <b>Recommended:</b> Use tools with care, return tools when not in use, 
+          notify your Site Coordinator or the Project Grow office when tools break, 
+          never discard hoses (leaks can be fixed!)
+        </p>
+      </div>
     </div>
     <div class="indent" id="guidelines-row">
       <div id="maintenance" onclick="print_guide(maintenance, maintenanceGuide)" class="story-box">
@@ -279,57 +295,50 @@
     </div>
     <!--Once again, put dropdowns here-->
     <div id="maintenanceGuide" class="guidelines-expansion hide left">
-      <div id="weed-top-text">
+      <div class="guideline">
         <p>
-          Controlling weeds not only helps your plants flourish, but it 
-          prevents neighbor garden plots from becoming overrun as well
+          This section is new
         </p>
         <p>
-          <b>Recommended:</b> Use hay to mulch around established seedlings 
-          then hand weed as needed (Note: many sites coordinate a hay 
-          delivery by a local farmer at the beginning of the season - 
-          ask your Site Coordinator for details)
+          <b>Recommended:</b> 
         </p>
         <p>
-          <b>Prohibited:</b> Herbicides of any kind as they are hazardous 
-          to human health, may kill or damage desirable crops, drift into 
-          neighboring gardens, or remain in the soil long after initial use
+          <b>Prohibited:</b>
         </p>
-        <div id="weed-bottom-text">
-          <div id="weed-fungicides">
-            <p>
-              <b>Allowed fungicides:</b></br>
-              Baking soda</br>
-              Liquid copper</br>
-              Anything else specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed fungicides:</b></br>
-              Anything not specifically labeled organic.
-            </p>
-          
-          </div>
-          <div id="weed-herbicides">
-            <p>
-              <b>Allowed herbicides:</b></br>
-              Anything specifically labeled organic.
-            </p>
-            <p>
-              <b>Disallowed herbicides:</b></br>
-              Round Up (glyphosate)</br>
-              Preen</br>
-              Anything not specifically labeled organic.
-            </p>
-          </div>
-        </div>
       </div>
     </div>
     <div id="waterGuide" class="guidelines-expansion hide middle">
-      water
+      <div class="guideline">
+        <p>Water costs are included in plot fees</p>
+        <p>
+          <b>Recommended:</b> Hand-held hose attachments for precise watering, 
+          mulching to conserve water by holding in moisture; contacting your 
+          Site Coordinator or the  Project Grow office if there's a leak or water problem
+        </p>
+        <p>
+          <b>Prohibited:</b> Unattended garden watering, soaker hoses, hose timers
+        </p>
+      </div>
     </div>
     <div id="closingGuide" class="guidelines-expansion hide right">
-      closing
+      <div class="guideline">
+        <p>
+          Gardeners must clean up plots (including perennial plots) at the end of each season. 
+          <i>The closing date is the third Saturday in October and ranges from the 15th to the 21st.</i>
+        </p>
+        <p>
+          <b>Required by Closing Day:</b> Annual plots ready for plowing, free of all non-organic material 
+          (e.g. fencing, stakes) with vegetation cut to 1-foot lengths and scattered around the plot; 
+          perennial plots should be free from all non-organic materials that are not in use and have a neat appearance
+        </p>
+      </div>
     </div>
+  </div>
+  <div id="guidelines-note-to-all" class="indent">
+    <p>
+      <b>PLEASE REMEMBER -</b> We are guests on all properties where garden sites are located. 
+      Our members and gardeners represent Project Grow. Please be a good tenant and respectful of landowners and neighbors.
+    </p>
   </div>
 </body>
 </html>
