21a7a7897a8f8f87153734869a01906004ead80e
Alvin Chen <alvinch@umich.edu>
2021-02-19 13:05:01 -0500

    Fixed styling for individual plots

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -55,5 +55,6 @@ function add_styles() {
   wp_enqueue_style("ourpeople", get_theme_file_uri('/css/ourpeople.css'));
   wp_enqueue_style( "plant-sale-info", get_theme_file_uri('/css/plant-sale-info.css'));
   wp_enqueue_style( "rental-information", get_theme_file_uri('/css/rental-info.css'));
+  wp_enqueue_style( "individual-plots", get_theme_file_uri('/css/indv-plots.css'));
 }
 add_action('wp_enqueue_scripts', 'add_styles');
