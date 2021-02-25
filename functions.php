57d7dc7199aa4a83245c3cf37cbaf280c8238c91
KavyaManocha <59888688+KavyaManocha@users.noreply.github.com>
2021-02-25 16:59:19 -0500

    FINISHED PHP for classes-events

--- functions.php
+++ functions.php
@@ -1,112 +1,119 @@
 <?php
-  require get_template_directory() . '/inc/example-post-type.php';
-  require get_template_directory() . '/inc/enqueue-scripts.php';
-  require get_template_directory() . '/inc/register-settings.php';
+    require get_template_directory() . '/inc/example-post-type.php';
+    require get_template_directory() . '/inc/enqueue-scripts.php';
+    require get_template_directory() . '/inc/register-settings.php';
+    require get_template_directory() . '/inc/customizer.php';
+    require get_template_directory() . '/inc/template_functions.php';
+    
 
-  // add_action('wp_head', 'classes_events_customize');
+  // function classes_events_customize($wp_customize) {
+  //   /*Added img just so I could style around it
+  //     Absolutely feel free to change variable names
+  //     I just needed the image to appear on the site*/
+  //   $wp_customize->add_section('classes-events-section', array(
+  //     'title' => 'Classes & Events'
+  //   ));
 
-  function classes_events_customize($wp_customize) {
-    $wp_customize->add_section('classes-events-section', array(
-      'title' => 'Classes & Events'
-    ));
-    // Start img
-    $wp_customize->add_setting('classes-events-img');
 
-    $wp_customize->add_control( new WP_Customize_Cropped_Image_control(
-      $wp_customize, 'classes-events-img-control', array(
-      'label' => 'Image',
-      'section' => 'classes-events-section',
-      'settings' => 'classes-events-img',
-      'width' => 250,
-      'height' => 250
-    )));
+  //   $wp_customize->add_setting('classes-events-img');
 
-    //Start Headline
-    $wp_customize->add_setting('classes-events-headline', array(
-      'default' => 'Headline'
-    ));
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-headline', array(
-      'label' => 'Headline', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-headline'
-    )));
+  //   $wp_customize->add_control( new WP_Customize_Cropped_Image_control(
+  //     $wp_customize, 'classes-events-img-control', array(
+  //     'label' => 'Image',
+  //     'section' => 'classes-events-section',
+  //     'settings' => 'classes-events-img',
+  //     'width' => 750,
+  //     'height' => 750
+  //   )));
 
-    $wp_customize->add_setting('classes-events-paragraph', 
-    array('default'=> 'Paragraph'));
 
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-paragraph', array(
-      'label' => 'Event Details', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-paragraph',
-      'type' => 'textarea'
-    )));
+  //   $wp_customize->add_setting('classes-events-headline', 
+  //   array('default'=> 'Example Headline Text'));
 
-    $wp_customize->add_setting('classes-events-location', 
-      array('default'=> 'Location'
-    ));
+  //   $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-headline', array(
+  //     'label' => 'Headline', 
+  //     'section' => 'classes-events-section', 
+  //     'settings' => 'classes-events-headline'
+  //     )));
+
+  //   $wp_customize->add_setting('classes-events-paragraph', 
+  //   array('default'=> 'Example Paragraph Text'));
+
+  //   $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-paragraph', array(
+  //     'label' => 'Text', 
+  //     'section' => 'classes-events-section', 
+  //     'settings' => 'classes-events-paragraph',
+  //     'type' => 'textarea'
+  //     )));
+
+  //     $wp_customize->add_setting('classes-events-location', 
+  //     array('default'=> 'Example Location Text'));
   
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-location', array(
-      'label' => 'Location', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-location',
-      'type' => 'textarea'
-    )));
+  //     $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-location', array(
+  //       'label' => 'Text', 
+  //       'section' => 'classes-events-section', 
+  //       'settings' => 'classes-events-location',
+  //       'type' => 'textarea'
+  //       )));
 
-    $wp_customize->add_setting('classes-events-event-type', 
-      array('default'=> 'Class or Event?'
-    ));
+  //     $wp_customize->add_setting('classes-events-event-type', 
+  //       array('default'=> 'Example Event Type Text'));
   
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-event-type', array(
-      'label' => 'Event type', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-event-type',
-      'type' => 'textarea'
-    )));
+  //     $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-event-type', array(
+  //       'label' => 'Text', 
+  //       'section' => 'classes-events-section', 
+  //       'settings' => 'classes-events-event-type',
+  //       'type' => 'textarea'
+  //       )));
 
-    $wp_customize->add_setting('classes-events-cost', 
-      array('default'=> '5.00'
-    ));
+  //     $wp_customize->add_setting('classes-events-cost', 
+  //       array('default'=> 'Example Cost Text'));
   
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-cost', array(
-      'label' => 'Cost', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-cost',
-      'type' => 'textarea'
-    )));
+  //     $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-cost', array(
+  //       'label' => 'Text', 
+  //       'section' => 'classes-events-section', 
+  //       'settings' => 'classes-events-cost',
+  //       'type' => 'textarea'
+  //       )));
 
-    $wp_customize->add_setting('classes-events-time', 
-      array('default'=> '00:00AM - 00:00PM'
-    ));
+  //     $wp_customize->add_setting('classes-events-time', 
+  //       array('default'=> '00:00 - 00:00'));
   
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-time', array(
-      'label' => 'Time', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-time',
-      'type' => 'textarea'
-    )));
+  //     $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-time', array(
+  //       'label' => 'Text', 
+  //       'section' => 'classes-events-section', 
+  //       'settings' => 'classes-events-time',
+  //       'type' => 'textarea'
+  //       )));
 
-    $wp_customize->add_setting('classes-events-date', 
-      array('default'=> 'Jan 01, 2021'
-    ));
+  //     $wp_customize->add_setting('classes-events-date',  $example_repeater,
+  //       array('default'=> 'Month day, year'));
+      
+      
+  
+  //    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-date', array(
+  //      'label' => 'Text',
+  //      'section' => 'classes-events-section',
+  //      'settings' => 'classes-events-date',
+  //      'type' => 'textarea'
+  //      )));
 
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-date', array(
-      'label' => 'Date', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-date',
-      'type' => 'textarea'
-    )));
+  //     $wp_customize->add_setting('classes-events-join-link');
+  
+  //     $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-join-link', array(
+  //       'label' => 'Link', 
+  //       'section' => 'classes-events-section', 
+  //       'settings' => 'classes-events-join-link',
+  //       'type' => 'dropdown-pages'
+  //       )));
+  // }
 
-    $wp_customize->add_setting('classes-events-join-link');
 
-    $wp_customize->add_control( new WP_Customize_Control($wp_customize, 'classes-events-callout-join-link', array(
-      'label' => 'Link', 
-      'section' => 'classes-events-section', 
-      'settings' => 'classes-events-join-link',
-      'type' => 'dropdown-pages'
-    )));
-  }
+  // add_action('customize_register', 'classes_events_customize');
 
-  add_action('customize_register', 'classes_events_customize');
+  // add callout section for section 1
+
+  
 
 ?>
 
---
+++ register-settings.php
@@ -0,0 +1,87 @@
+<?php
+// Example of how to use a repeatable box
+register_nav_menu("primary", "Top Navbar");
+
+
+
+  function example_repeatable_customizer($wp_customize) {
+    require 'section_vars.php';  
+    require_once 'controller.php';
+    
+    $wp_customize->add_section($example_section, array(
+      'title' => 'Classes & Events Repeater',
+    ));
+    
+    $wp_customize->add_setting(
+      $example_repeater,
+      array(
+          'sanitize_callback' => 'onepress_sanitize_repeatable_data_field',
+          'transport' => 'refresh',
+      ) );
+  
+    $wp_customize->add_control(
+        new Onepress_Customize_Repeatable_Control(
+            $wp_customize,
+            $example_repeater,
+            array(
+                'label' 		=> esc_html__('Classes & Events Repeater'),
+                'description'   => '',
+                'section'       => $example_section,
+                'live_title_id' => 'question',
+                'title_format'  => esc_html__('[live_title]'), // [live_title]
+                'max_item'      => 10, // Maximum item can add
+                'limited_msg' 	=> wp_kses_post( __( 'Max items added' ) ),
+                'fields'    => array(
+                    'Image'  => array(
+                        'title' => esc_html__('Image'),
+                        'type'  =>'media',
+                    ),
+                    'Headline'  => array(
+                        'title' => esc_html__('Headline'),
+                        'type'  =>'text',
+                    ),
+                    'Location'  => array(
+                        'title' => esc_html__('Location'),
+                        'type'  =>'text',
+                    ),
+                    'Event_Type'  => array(
+                        'title' => esc_html__('Event Type'),
+                        'type'  =>'text',
+                    ),
+                    'Event_Date'  => array(
+                        'title' => esc_html__('Event Date'),
+                        'type'  =>'text',
+                    ),
+                    'Paragraph'  => array(
+                        'title' => esc_html__('Description'),
+                        'type'  =>'textarea',
+                    ),
+                    'Cost'  => array(
+                        'title' => esc_html__('Cost'),
+                        'type'  =>'text',
+                    ),
+                    'Time'  => array(
+                        'title' => esc_html__('00:00XX-00:00XX'),
+                        'type'  =>'text',
+                    ),
+                    'Link'  => array(
+                        'title' => esc_html__('Link'),
+                        'type'  =>'url',
+                    ),
+                ),
+            )
+        )
+    );
+  }
+  add_action( 'customize_register', 'example_repeatable_customizer' );
+
+
+
+
+
+
+
+
+
+
+?>
