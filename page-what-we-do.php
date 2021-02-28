46872ec3a7850b5751283e6cc6b21ffda68ed450
Christopher Cooley <clcooley@umich.edu>
2021-02-27 20:57:59 -0500

    Adds all content to page What We do
    Next steps include
    figuring out why we can only use part of the width of page
    --This is likely due to conflicting css files, but cant continue until resolved
    fixing the spacing between sections and moving the green circles to their correct places (last)
    figuring out why the pictures do not load

--- page-what-we-do.php
+++ page-what-we-do.php
@@ -9,7 +9,149 @@
         <button class="wwd-newsletter-btn"><b>Sign Up for our Newsletter</b></button>
     </div>
 </div>
-<div class="wwd-indent wwd-main-page wwd-title"><b>Our Roots</b></div>
+<div class="wwd-indent wwd-new-section wwd-title"><b>Our Roots</b></div>
+<div class="wwd-indent wwd-description">
+    <div class="wwd-pg-space">
+        Since 1972, Project Grow has been facilitating commtunity gardens throughout 
+        Ann Arbor, making it the longest standing community garden organization in the 
+        nation. The idea of community gardens, however, dates back even farther.
+    </div>
+    <div class="wwd-pg-space">
+        In the years surrounding the World Wars, citizens in many countries affected by the wars 
+        planted vegetables to help make up for food shortages. These "Victory Gardens," as 
+        they were often called, found home on public land as their objective was a civic 
+        one that supported both the war effort and the troops abroad. Despite their success 
+        and popularity,  after the conclusion of World War II when the US has returned to 
+        prosperity and surplus, the original community gardens mostly died out. 
+    </div>
+    <div class="wwd-pg-space">
+        With the 1970s came new environmental awareness and the resurgence of the ideals of community 
+        growing space. In 1972, a group of  Ann Arbor residents organized a handful of gardens 
+        that were permitted to use public space. Now, many years later, Project Grow has built 
+        upon this foundation and each year facilitates 20 individual sites across the community, 
+        providing over 350 individual plots. The organization has also expanded its reach, 
+        teaching classes about topics related to vegetable gardening, providing free plots for 
+        low income families, and much more. Learn more about Project Grow's full range of activities!
+    </div>
+</div>
+    <div id="wwd-super-title" class="wwd-indent">History of Project Grow</div>
+<div id="wwd-flex-container" class="wwd-indent">
+    <div id="wwd-left-side-container">
+        <div>
+            <div class="wwd-title wwd-right-align">1971</div>
+            <div class="wwd-pg-space">
+                In the years surrounding the World Wars, citizens in many countries affected by the wars 
+                planted vegetables to help make up for food shortages. These "Victory Gardens," as they 
+                were often called, found home on public land as their objective was a civic one that supported 
+                both the war effort and the troops abroad. Despite their success and popularity,  after the 
+                conclusion of World War II when the US has returned to prosperity and surplus, the original 
+                community gardens mostly died out.
+            </div>
+            <div class="wwd-pg-space">
+                Project Grow was founded by Susan Drake, a resident of the Burns Park neighborhood of Ann Arbor. 
+                Susan wanted to start a “Victory Garden” for herself and some neighbors using public land. She 
+                arranged to use some undeveloped land at Stone School and Ellsworth until it was sold for development. 
+                Water for the site was drawn from a nearby creek. About one acre of land was available. This was more 
+                space than the Burns Park gardeners needed, so Susan invited residents of Arbor Park (now University 
+                Townhouses), a nearby low income housing development, to join them.  Altogether in 1971, 43 families 
+                gardened together at the Stone School site, about half from Burns Park and half from Arbor Park. 
+            </div>
+            <div class="wwd-pg-space">
+                The success of the first year’s garden convinced Susan Drake that community gardening should be available 
+                city wide. And it was to be more than simply gardening. The joining together in a single effort of affluent 
+                Burns Park residents and the low income and more diverse residents of Arbor Park was a model that should be replicated.
+            </div>
+        </div>
+        <div>
+        <img id="wwd-1972-left" src="images/wwd-1972(left).png" alt="1972 pic left">
+        <img id="wwd-1972-right" src="images/wwd-1972(right).png" alt="1972 pic right">
+        </div>
+        <div class="wwd-title wwd-right-align">Early to Mid  1970s</div>
+        <div>
+            <div class="wwd-pg-space">
+                Community gardening continued to be popular during the early to mid 1970s, but Project Grow suffered from an annual 
+                turnover of staff. There was no continuity and little institutional memory. Each year the whole effort had to be 
+                virtually re-invented. This improved in the late 1970s when the City of Ann Arbor added a line item in their budget 
+                to support Project Grow. This allowed Project Grow to hire a year-around employee.
+            </div>
+        </div>
+        <div>
+            <img id="wwd-1980" src="images/wwd-1980s-2000s.png" alt="1980s-2000s pic">
+        </div>
+        <div class="wwd-title wwd-right-align">2010s</div>
+        <div>
+            <div class="wwd-pg-space">
+                The garden at County Farm Park is still there nearly 50 years later and the garden at Zion Lutheran Church 
+                was re-created in 2016 after being removed for several years to accomodate a church expansion. 
+            </div>
+        </div>
+        <div>
+            <img id="wwd-present" src="images/wwd-present.png" alt="present picture">
+        </div>
+    </div>
+    <!-- This is the code for that green thing down the middle -->
+    <div id="wwd-page-design">   
+        <svg width="50" height="3000">
+            <circle cx="25" cy="25" r="10" stroke="green" stroke-width="4" fill="green" />
+            <line x1="25" y1="25" x2="25" y2="3000" stroke="green" stroke-width="6" />
+            <circle cx="25" cy="250" r="10" stroke="green" stroke-width="4" fill="green" />
+            <circle cx="25" cy="500" r="10" stroke="green" stroke-width="4" fill="green" />
+            <circle cx="25" cy="750" r="10" stroke="green" stroke-width="4" fill="green" />
+            <circle cx="25" cy="1000" r="10" stroke="green" stroke-width="4" fill="green" />
+            <circle cx="25" cy="1250" r="10" stroke="green" stroke-width="4" fill="green" />
+        </svg>
+    </div>
+    <div id="wwd-right-side-container" class="wwd-pg-space">
+        <div>
+            <img id="wwd-1971" src="images/wwd-1971.png" alt="1971 picture">
+        </div>
+        <div class="wwd-title">1972</div>
+        <div>
+            In 1972, Susan became a missionary for community gardening. She spoke to Ann Arbor City 
+            Council, the Ann Arbor School Board, the Recreation Advisory Committee, the Washtenaw County 
+            Board of Commissioners, the Ecology Center, the Chamber of Commerce and others promoting the idea. 
+            Susan lined up additional garden sites on public, private and church properties.  New sites 
+            included Arrowwood Hills on Pontiac Trail, County Farm Park, Freeman School in Dixboro, Peace Neighborhood 
+            Center, Zion Lutheran Church and Hikone Public Housing. Project Grow developed bylaws and created a gardener 
+            elected governing board and became a 501(c)3 in 1972.
+        </div>
+        <div>
+            <img id="wwd-mid1970s" src="images/wwd-mid1970s.png" alt="mid 1970s picture">
+        </div>
+        <div class="wwd-title">1980s - 2000s</div>
+        <div>
+            City support for Project Grow allowed the organization to strengthen during the 80s and 90s. City support was 
+            gradually reduced after that and eliminated in 2009. However, the municipal support allowed Project Grow to develop 
+            a strong core of gardeners and volunteers. 
+        </div>
+        <div>
+            <img id="wwd-2010s" src="images/wwd-2010s.png" alt="2010s picture">
+        </div>
+        <div class="wwd-title">Present</div>
+        <div>
+            Now, many years later, Project Grow has built upon this foundation and operates 20 
+            individual sites across the community, serving more than 400 gardeners each year. 
+        </div>
+        <div>
+            The organization has also expanded its reach to include:
+        </div>
+        <div>
+            • Teaching classes about topics related to organic gardening.
+        </div>
+        <div>
+            • Providing reduced fee plots for low income families.
+        </div>
+        <div>
+            • Administering the Master Composter education classes.
+        </div>
+        <div>
+            • Offering more than 100 varieties of heirloom tomatoes, peppers and basil at an annual sale.
+        </div>
+        <div>
+            • Holding an annual heirloom tomato tasting so tomato lovers can choose their favorites among dozens of varieties.
+        </div>
+    </div>
+</div>
 
 <?php 
   get_footer();
